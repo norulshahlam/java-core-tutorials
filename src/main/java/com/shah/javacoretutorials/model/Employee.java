1eaa6f3a832715de39d2f72da74fcffbba47d5f1
norulshahlam <norulshahlam@gmail.com>
2022-05-12 21:50:04 +0800

    third rule

--- src/main/java/com/shah/javacoretutorials/model/Employee.java
+++ src/main/java/com/shah/javacoretutorials/model/Employee.java
@@ -2,9 +2,11 @@ package com.shah.javacoretutorials.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     private String name;
     private int age;
