bc597d6455d5c00cd5945e87b5b7b9534e501e91
norulshahlam <norulshahlam@gmail.com>
2022-09-02 23:22:04 +0800

    LargestFiveDigitNumbersTest

---
+++ src/test/java/com/shah/javacoretutorials/codewars/LargestFiveDigitNumbersTest.java
@@ -0,0 +1,45 @@
+package com.shah.javacoretutorials.codewars;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Integer.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.max;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class LargestFiveDigitNumbersTest {
+
+    /**
+     * n the following 6 digit number: 283910
+     * 91 is the greatest sequence of 2 consecutive digits.
+     * <p>
+     * In the following 10 digit number: 1234567890
+     * 67890 is the greatest sequence of 5 consecutive digits.
+     * <p>
+     * Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+     */
+    @Test
+    public void exampleTests1() {
+        assertThat(solve("283910")).isEqualTo(83910);
+    }
+
+    @Test
+    public void exampleTests2() {
+        assertThat(solve("1234567890")).isEqualTo(67890);
+    }
+
+    @Test
+    public void exampleTests3() {
+        assertThat(solve("731674765")).isEqualTo(74765);
+    }
+
+    public static int solve(final String digits) {
+        int largest = MIN_VALUE;
+        for (int i = 0; i <= digits.length() - 5; i++) {
+            int number = parseInt(digits.substring(i, i + 5));
+            largest = max(number, largest);
+        }
+        return largest;
+    }
+}
--- src/test/java/com/shah/javacoretutorials/codewars/SelectionSort.java
+++ src/test/java/com/shah/javacoretutorials/codewars/SelectionSort.java
@@ -2,22 +2,24 @@ package com.shah.javacoretutorials.codewars;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class SelectionSort {
 
     @Test
-    void contextLoads() {
+    void sortNumbers() {
         int[] numberList = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
         for (int b : numberList)
             System.out.print(b + ", ");
         System.out.print("\n");
-        sort(numberList);
+        selectionSort(numberList);
         for (int b : numberList)
             System.out.print(b + ", ");
-
+        assertThat(numberList).contains(1, 2);
     }
 
-    public static void sort(final int[] arr) {
+    public static void selectionSort(final int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int minElementIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -25,7 +27,6 @@ public class SelectionSort {
                     minElementIndex = j;
                 }
             }
-
             if (minElementIndex != i) {
                 int temp = arr[i];
                 arr[i] = arr[minElementIndex];
@@ -33,4 +34,5 @@ public class SelectionSort {
             }
         }
     }
+
 }
