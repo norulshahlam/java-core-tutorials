ecaad41a5da74ee9e1a6c9ab62f27eeba9c785f7
norulshahlam <norulshahlam@gmail.com>
2023-01-09 21:27:12 +0800

    added parallel streams

---
+++ src/main/java/com/shah/javacoretutorials/util/CommonUtil.java
@@ -0,0 +1,56 @@
+package com.shah.javacoretutorials.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.StopWatch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
+import static java.lang.Thread.sleep;
+
+@Slf4j
+public class CommonUtil {
+
+    public static StopWatch stopWatch = new StopWatch();
+
+    public static void startTimer() {
+        stopWatchReset();
+        stopWatch.start();
+    }
+
+    public static void timeTaken() {
+        stopWatch.stop();
+        log.info("Total Time Taken : " + stopWatch.getTime());
+    }
+
+    public static void delay(long delayMilliSeconds) {
+        try {
+            sleep(delayMilliSeconds);
+        } catch (Exception e) {
+            log.info("Exception is :" + e.getMessage());
+        }
+
+    }
+
+    public static void stopWatchReset() {
+        stopWatch.reset();
+    }
+
+    public static ArrayList<Integer> generateArrayList(int maxNumber) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        IntStream.rangeClosed(1, maxNumber)
+                .boxed()
+                .forEach((arrayList::add));
+        return arrayList;
+    }
+
+    public static LinkedList<Integer> generateIntegerLinkedList(int maxNumber) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        IntStream.rangeClosed(1, maxNumber)
+                .boxed()
+                .forEach((linkedList::add));
+        return linkedList;
+    }
+}
