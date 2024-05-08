d124b92d53afcfdc8c5374fc45a084a42aec1c70
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-05-08 22:14:55 +0800

    Update RaceCondition.java

--- src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/RaceCondition.java
+++ src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/RaceCondition.java
@@ -1,8 +1,9 @@
 package com.shah.javacoretutorials.tutorials.advance.multithreading;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
@@ -13,13 +14,17 @@ https://www.linkedin.com/pulse/understanding-race-conditions-causes-prevention-p
 
 A race condition occurs when two or more threads can access shared data, and they try to change it at the same time. Because the thread scheduling algorithm can swap between threads at any time, you don't know the order in which the threads will attempt to access the shared data. Therefore, the result of the change in data is dependent on the thread scheduling algorithm, i.e., both threads are "racing" to access/change the data.
 
+Below is an example of a race condition and how to fix the race condition.
+
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RaceCondition {
 
     private int count = 0;
 
     @RepeatedTest(10)
-    @DisplayName("Negative example")
+    @Order(1)
+    @DisplayName("negative Sample Using Runnable")
     void negativeSampleUsingRunnable() throws InterruptedException {
 
         /*
@@ -41,7 +46,8 @@ class RaceCondition {
     }
 
     @RepeatedTest(10)
-    @DisplayName("Positive example")
+    @Order(2)
+    @DisplayName("positive Sample Using Runnable")
     void positiveSampleUsingRunnable() throws InterruptedException {
         AtomicInteger countAtomic = new AtomicInteger(0);
 
@@ -60,6 +66,41 @@ class RaceCondition {
         increaseCount1.join();
         increaseCount2.join();
 
+        System.out.println(countAtomic);
+    }
+
+    @RepeatedTest(10)
+    @Order(3)
+    @DisplayName("negative Sample Using Executor")
+    void negativeSampleUsingExecutor() {
+         /*
+        The expected value should be 2000, but it gave different results for every run.
+         */
+        ArrayList<CompletableFuture<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> count++);
+            list.add(future);
+        }
+        list.forEach(CompletableFuture::join);
+        System.out.println(count);
+    }
+
+    @RepeatedTest(10)
+    @Order(4)
+    @DisplayName("positive Sample Using Executor")
+    void positiveSampleUsingExecutor() {
+          /*
+        The expected value should be 2000, and will always give 2000 for every run.
+         */
+        AtomicInteger countAtomic = new AtomicInteger(0);
+        ArrayList<CompletableFuture<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(countAtomic::incrementAndGet);
+            list.add(future);
+        }
+        list.forEach(CompletableFuture::join);
         System.out.println(countAtomic);
     }
 }
