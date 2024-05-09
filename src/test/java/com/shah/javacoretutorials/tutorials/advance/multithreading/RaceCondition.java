efea62daf60eb0d9895ec08696afc9feafaa7502
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-05-09 14:04:20 +0800

    Update RaceCondition.java

--- src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/RaceCondition.java
+++ src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/RaceCondition.java
@@ -12,6 +12,8 @@ import java.util.stream.IntStream;
 https://www.youtube.com/watch?v=3vptroRUvn8
 https://www.linkedin.com/pulse/understanding-race-conditions-causes-prevention-pavan-pothuganti-9z61c/
 
+Thread safe means that multiple threads can use a method or class instance at the same time without any problems occurring.
+
 A race condition occurs when two or more threads can access shared data, and they try to change it at the same time. Because the thread scheduling algorithm can swap between threads at any time, you don't know the order in which the threads will attempt to access the shared data. Therefore, the result of the change in data is dependent on the thread scheduling algorithm, i.e., both threads are "racing" to access/change the data.
 
 Below is an example of a race condition and how to fix it (ensure thread-safe):
@@ -19,7 +21,7 @@ Below is an example of a race condition and how to fix it (ensure thread-safe):
 1. AtomicInteger - use this class as ref and use incrementAndGet() to count
 2. synchronized - use this keyword in your method. It only calls by 1 thread at a time. If t1 is executed, t2 have to wait.
 
-We also we can create thread using anonymous class instead of creating a new class n extends thread
+We also create thread using anonymous class instead of creating a new class n extends thread
 
  */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -27,10 +29,14 @@ class RaceCondition {
 
     private int count = 0;
 
+    private synchronized void synchronizedIncrement() {
+        this.count++;
+    }
+
     @RepeatedTest(10)
     @Order(1)
-    @DisplayName("negative Sample Using Runnable")
-    void negativeSampleUsingRunnable() throws InterruptedException {
+    @DisplayName("negative Sample Using Runnable and Thread")
+    void negativeSampleUsingRunnableAndThread() throws InterruptedException {
 
         /*
         The expected value should be 2000, but it gave different results for every run.
@@ -52,8 +58,8 @@ class RaceCondition {
 
     @RepeatedTest(10)
     @Order(2)
-    @DisplayName("positive Sample Using Runnable")
-    void positiveSampleUsingRunnable() throws InterruptedException {
+    @DisplayName("positive Sample Using thread Anonymous Class Atomic")
+    void positiveSampleUsingThreadAnonymousClassAtomic() throws InterruptedException {
         AtomicInteger countAtomic = new AtomicInteger(0);
 
         /*
@@ -76,6 +82,27 @@ class RaceCondition {
 
     @RepeatedTest(10)
     @Order(3)
+    @DisplayName("positive Sample Using thread anonymous and Synchronized")
+    void positiveSampleUsingThreadAnonymousSynchronized() throws InterruptedException {
+        /*
+        The expected value should be 2000, and will always give 2000 for every run.
+         */
+        Thread increaseCount1 = new Thread(() -> IntStream
+                .range(0, 10000).forEach(i -> synchronizedIncrement()));
+        Thread increaseCount2 = new Thread(() -> IntStream
+                .range(0, 10000).forEach(i -> synchronizedIncrement()));
+
+        increaseCount1.start();
+        increaseCount2.start();
+
+        increaseCount1.join();
+        increaseCount2.join();
+
+        System.out.println(count);
+    }
+
+    @RepeatedTest(10)
+    @Order(4)
     @DisplayName("negative Sample Using Executor")
     void negativeSampleUsingExecutor() {
          /*
@@ -92,9 +119,9 @@ class RaceCondition {
     }
 
     @RepeatedTest(10)
-    @Order(4)
-    @DisplayName("positive Sample Using Executor")
-    void positiveSampleUsingExecutor() {
+    @Order(5)
+    @DisplayName("positive Sample Using Executor Atomic")
+    void positiveSampleUsingExecutorAtomic() {
           /*
         The expected value should be 2000, and will always give 2000 for every run.
          */
