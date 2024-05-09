10627c3f627a5945940dc9bdeb4fbdb81f02d3cc
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-05-09 22:10:34 +0800

    Update AtomicVolatileSynchronized.java

--- src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/AtomicVolatileSynchronized.java
+++ src/test/java/com/shah/javacoretutorials/tutorials/advance/multithreading/AtomicVolatileSynchronized.java
@@ -6,6 +6,25 @@ import org.junit.jupiter.api.Test;
 
 https://www.youtube.com/watch?v=3vptroRUvn8
 
+Synchronized
+For executing on Methods/blocks
+Can be used to implement lock-based algorithms
+Slow because of acquire and release of lock
+No possibility of deadlock and livelock
+
+Volatile
+For read / write variables
+Can be used to implement Non-lock-based algorithms
+Relatively faster than Synchronized
+Possibility of deadlock and livelock
+
+Atomic
+For read / write / modify variables
+Can be used to implement Non-lock-based algorithms
+Relatively faster than Synchronized and Volatile
+Possibility of deadlock and livelock
+
+
  */
 class AtomicVolatileSynchronized {
     @Test
