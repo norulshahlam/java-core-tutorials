fe27fcc9447a169fac022c50665d36b8ac76c8b2
norulshahlam <norulshahlam@gmail.com>
2025-12-06 21:30:01 +0800

    refactor: update SuperKeyword2 class and improve constructor tests

--- src/test/java/com/shah/javacoretutorials/tutorials/beginnerImpt/SuperKeyword2.java
+++ src/test/java/com/shah/javacoretutorials/tutorials/beginnerImpt/SuperKeyword2.java
@@ -2,53 +2,72 @@ package com.shah.javacoretutorials.tutorials.beginnerImpt;
 
 import org.junit.jupiter.api.Test;
 
-/*
-The super keyword in java is a reference variable used to refer parent class objects.
-The keyword “super” came into the picture with the concept of Inheritance.
-
-If a constructor does not explicitly invoke a superclass constructor, the Java compiler automatically inserts a call to the no-argument constructor of the superclass.
-
-In this case, if you create an object of child class, the parent class constructor will be called first.
+/**
+ * The super keyword in java is a reference variable used to refer parent class objects.
+ * The keyword “super” came into the picture with the concept of Inheritance.
+ * If a constructor does not explicitly invoke a superclass constructor, the Java compiler automatically inserts a call to the no-argument constructor of the superclass.
+ * In this case, if you create an object of child class, the parent class constructor will be called first.
  */
+@SuppressWarnings("ALL")
 class SuperKeyword2 {
-    @SuppressWarnings("unused")
+
     @Test
-    void test() {
+    void test1() {
         // run this test to see which constructor gets called first
-        Daughter d = new Daughter();
+        Father d = new Father(2);
     }
+
     @Test
     void test2() {
         // run this test to see which constructor gets called first
-        Daughter d = new Daughter(2);
+        Son d = new Son();
     }
+
     @Test
     void test3() {
         // run this test to see which constructor gets called first
-        Mother d = new Mother(2);
+        GrandSon d = new GrandSon();
+    }
+
+    @Test
+    void test4() {
+        // run this test to see which constructor gets called first
+        GrandSon d = new GrandSon(2);
     }
 }
 
-class Mother {
-    public Mother() {
+class Father {
+    public Father() {
         // by default there is super() here
-        System.out.println("from Mother");
+        System.out.println("from Father");
     }
 
-    public Mother(int c) {
+    public Father(int c) {
         // by default there is super() here
-        System.out.println("from para Mother: " + c);
+        System.out.println("from para Father: " + c);
     }
 }
 
-class Daughter extends Mother {
-    public Daughter() {
+class Son extends Father {
+    public Son() {
         // by default there is super() here will call to the no-argument constructor of the superclass.
-        System.out.println("from Daughter");
+        System.out.println("from Son");
     }
 
-    public Daughter(int d) {
+    public Son(int d) {
         // by default there is super() here will call to the no-argument constructor of the superclass.
-        System.out.println("from para Daughter: " + d);
+        System.out.println("from para Son: " + d);
+    }
+}
+
+class GrandSon extends Son {
+    public GrandSon() {
+        // by default there is super() here will call to the no-argument constructor of the superclass.
+        System.out.println("from GrandSon");
+    }
+
+    public GrandSon(int d) {
+        // by default there is super() here will call to the no-argument constructor of the superclass.
+        System.out.println("from para GrandSon: " + d);
     }
 }
